e8c715784c59703b1399cd74aeff27cda67949a7
loefflerjakob <jakob-loeffler@gmx.at>
2025-05-18 20:48:10 +0200

    showcase of fetching nutritonal goal + goal type

---
+++ app/types/goal.ts
@@ -0,0 +1,10 @@
+export type NutritionalGoal = {
+  id: string;
+  identifier: string;
+  calories: number;
+  protein: number;
+  carbohydrates: number;
+  fats: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,12 +1,29 @@
 "use client"
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState } from "react";
 import type { Entry } from '@/app/types/entry'
+import type { NutritionalGoal } from '@/app/types/goal'
 
 const Dashboard: React.FC = () => {
 
   const [entries, setEntries] = useState<Entry[]>([])
+  const [goal, setGoal] = useState<NutritionalGoal>()
 
-
+  useEffect(() => {
+    const fetchGoal = async () => {
+      try {
+        const res = await fetch('/api/goal')
+        if (!res.ok) {
+          throw new Error('Failed to fetch goal')
+        }
+        const data = await res.json()
+        setGoal(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchGoal()
+  }
+  , [])
 
     useEffect(() => {
       const fetchEntries = async () => {
@@ -25,10 +42,20 @@ const Dashboard: React.FC = () => {
       fetchEntries()
     }, [])
 
+    console.log(JSON.stringify(goal, null, 2))
     console.log(JSON.stringify(entries, null, 2))
 
   return (
     <div>
+      <h2>Your Nutritional Goals</h2>
+      {goal && (
+        <div>
+          <p>Calories: {goal.calories}</p>
+          <p>Protein: {goal.protein}</p>
+          <p>Carbohydrates: {goal.carbohydrates}</p>
+          <p>Fats: {goal.fats}</p>
+        </div>
+      )}
     </div>
   );
 };
